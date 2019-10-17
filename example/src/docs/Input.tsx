c188341cc3c31c560d4d82b059ec40514e8bbef3
Tytgat Stijn <stijn.tytgat@consultant.volvo.com>
2019-10-17 15:49:42 +0200

    typescript * Added example for layouting on inputs * Added text for inputs

--- example/src/docs/Input.tsx
+++ example/src/docs/Input.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { route } from 'navi'
 import { __, prop, assoc } from 'ramda'
-import { Heading, Flex, Input, TextArea, Select } from '../ui'
+import { Heading, Flex, Input, TextArea, Select, Text } from '../ui'
 import { MaxWidthBox } from '../ui'
 import { useState } from 'react'
 import { ExampleDocumentation } from './Doc'
 import { SyntheticEvent } from 'react'
+import { Accent } from '../components/Accent'
 
 const useInputs = () => {
   const [state, setState] = useState<{ [name: string]: string }>({})
@@ -22,6 +23,34 @@ const InputDocs = (props: {}) => {
       <MaxWidthBox>
         <Flex py={3}>
           <Heading>Input</Heading>
+        </Flex>
+        <Flex pb={3} width={{ sm: 1, lg: 1 / 2 }} flexDirection="column">
+          <Text mb={1}>
+            Inputs are an essential component for user-interaction. On top of
+            looking pretty and being developer-friendly, they should clearly
+            communicate their state and fit in the layout seamlessly.
+          </Text>
+          <Text mb={1}>
+            The states the inputs handle and visualize themselves are the
+            <Accent>normal</Accent>, <Accent>hover</Accent> and{' '}
+            <Accent>focused</Accent> states.
+            <br />
+            Other states that are supported, but you need to pass through
+            yourself, are the error state, and any other state you might need.
+            Passing a truthy <Accent>error-prop</Accent> will indicate a faulty
+            value, and by customizing <Accent>border/outline</Accent> colors,
+            you can display a success- or warn-state yourself. <br />
+            The <Accent>highlight</Accent> prop can be used to display this
+            state even when not focused or hovered.
+          </Text>
+          <Text mb={1}>
+            You can optionally pass a <Accent>label</Accent> to the provided
+            input components. These labels won't be rendered if you omit them.
+          </Text>
+          <Text>
+            Layout-wise, all props a regular Flex-component would support, can
+            also be used for these Input-components.
+          </Text>
         </Flex>
       </MaxWidthBox>
       <ExampleDocumentation first center title="Simple text input">
@@ -90,6 +119,31 @@ const InputDocs = (props: {}) => {
           <option>Noodles</option>
           <option>Beer</option>
         </Select>
+      </ExampleDocumentation>
+      <ExampleDocumentation center title="Layout props">
+        <>
+          <Input
+            width={1}
+            name="layout-1"
+            value={value('layout-1')}
+            onChange={e => onChange(e)}
+            placeholder="Full width input"
+          />
+          <Input
+            alignSelf="flex-end"
+            name="layout-2"
+            value={value('layout-2')}
+            onChange={e => onChange(e)}
+            placeholder="Flex item input"
+          />
+          <Input
+            my={3}
+            name="layout-3"
+            value={value('layout-3')}
+            onChange={e => onChange(e)}
+            placeholder="Spaced input"
+          />
+        </>
       </ExampleDocumentation>
     </Flex>
   )
