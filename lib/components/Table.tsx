0991f9dd2febf1ffadf08df66a647e86a9db0582
Stijn Tytgat <stinodesigns@gmail.com>
2019-11-25 12:09:12 +0100

    Fixed table components' types

--- lib/components/Table.tsx
+++ lib/components/Table.tsx
@@ -1,7 +1,11 @@
 import { color, LayoutProps, SpaceProps } from 'styled-system'
 import { styled, getColor, StyledComponent } from '../utils'
 import { Box } from './Box'
-import { HTMLAttributes } from 'react'
+import {
+  HTMLAttributes,
+  ComponentPropsWithoutRef,
+  DetailedHTMLProps,
+} from 'react'
 
 export const Table = styled(Box.withComponent('table'))({
   borderCollapse: 'collapse',
@@ -9,9 +13,9 @@ export const Table = styled(Box.withComponent('table'))({
 Table.displayName = 'Table'
 
 export const Row: StyledComponent<
-  HTMLAttributes<HTMLTableRowElement>,
-  { border?: boolean }
-> = styled(Box.withComponent('tr'))<{ border?: boolean }>(
+  DetailedHTMLProps<HTMLAttributes<HTMLTableRowElement>, HTMLTableRowElement>,
+  { border?: boolean } & ComponentPropsWithoutRef<typeof Box>
+> = styled(Box.withComponent('tr') as any)<{ border?: boolean }>(
   ({ theme, border }) => ({
     borderSpacing: 0,
     'td, th': {
@@ -23,9 +27,9 @@ export const Row: StyledComponent<
 )
 Row.displayName = 'Row'
 export const Cell: StyledComponent<
-  HTMLAttributes<HTMLTableCellElement>,
-  LayoutProps & SpaceProps & { bg?: string; color?: string }
-> = styled(Box.withComponent('td'))(
+  DetailedHTMLProps<HTMLAttributes<HTMLTableCellElement>, HTMLTableCellElement>,
+  ComponentPropsWithoutRef<typeof Box>
+> = styled(Box.withComponent('td') as any)(
   {
     whiteSpace: 'nowrap',
     textAlign: 'left',
@@ -42,9 +46,12 @@ export const Cell: StyledComponent<
 )
 Cell.displayName = 'Cell'
 export const Header: StyledComponent<
-  HTMLAttributes<HTMLTableHeaderCellElement>,
-  LayoutProps & SpaceProps & { bg?: string; color?: string }
-> = styled(Box.withComponent('th'))(
+  DetailedHTMLProps<
+    HTMLAttributes<HTMLTableHeaderCellElement>,
+    HTMLTableHeaderCellElement
+  >,
+  ComponentPropsWithoutRef<typeof Box>
+> = styled(Box.withComponent('th') as any)(
   {
     lineHeight: '28px',
     padding: '0 10px',
